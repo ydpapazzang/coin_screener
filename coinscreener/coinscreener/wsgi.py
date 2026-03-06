2b7e2a3d8835c91d22af5935b7e556b042af7d62
Saju Master <i.am.psw86@gmail.com>
2026-03-06 19:04:09 +0100

    json

--- coinscreener/coinscreener/wsgi.py
+++ coinscreener/coinscreener/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coinscreener.settings')
 
 application = get_wsgi_application()
+
+# 기존 코드 아래에 추가
+app = application
