d61dae9590638a0ce3565cede29bb5133957a8e0
Saju Master <i.am.psw86@gmail.com>
2026-03-06 11:40:26 +0100

    Add admin interfaces for Strategy and Condition models

--- coinscreener/screener/admin.py
+++ coinscreener/screener/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Strategy, Condition
 
-# Register your models here.
+class ConditionInline(admin.TabularInline):
+    model = Condition
+    extra = 1
+
+@admin.register(Strategy)
+class StrategyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    inlines = [ConditionInline]
+
+@admin.register(Condition)
+class ConditionAdmin(admin.ModelAdmin):
+    list_display = ('strategy', 'timeframe', 'offset', 'left_indicator', 'operator', 'right_indicator')
